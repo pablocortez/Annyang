c49e9097db87ac4ea48a30dac413f1c1227b9a05
Pablo Cortez <pabloenoc@live.com>
2015-10-31 16:41:42 +0000

    Fixed cursor. Voice commands need work.

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,28 @@
 $(function () {
-    $('#start').click(function () {
-        annyang.start();
 
-        if (annyang) {
-            var commands = {
-                'what is art': function () {
-                    $('#start').slideUp();
-                    $(".answer-1").typed({
-                        strings: ["Good question.", "Art is the highest expression of human intellect."],
-                        typeSpeed: 0
-                    });
-                }
-            };
+    annyang.start();
 
-            annyang.addCommands(commands);
-        }
-    });
+    if (annyang) {
+        var commands = {
+            'say hello': function () {
+                $(".hello").typed({
+                    strings: ["Hello!", "I am a project created by Pablo for Mrs. Guild's AP Lang class."],
+                    typeSpeed: 0
+                });
+            },
+            'what is art': function () {
+                $(".hello").empty();
+                $(".answer-1").typed({
+                    strings: ["Good question.", "Art is the highest expression of human intellect."],
+                    typeSpeed: 0
+                });
+            },
+            'clear screen': function () {
+                $(".answers").empty();
+            }
+        };
+
+        annyang.addCommands(commands);
+    }
+
 });
