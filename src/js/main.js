63419b4150793533e73f134e801a7722829c2d26
Pablo Cortez <pabloenoc@live.com>
2015-11-02 14:38:15 -0800

    Answer all questions

--- src/js/main.js
+++ src/js/main.js
@@ -5,20 +5,42 @@ $(function () {
     if (annyang) {
         var commands = {
             'say hello': function () {
-                $(".hello").typed({
+                $("#hello").typed({
                     strings: ["Hello!", "I am a project created by Pablo for Mrs. Guild's AP Lang class."],
                     typeSpeed: 0
                 });
             },
-            'what is art': function () {
-                $(".hello").empty();
+            'answer the questions' : function() {
+                $(".typed-cursor").fadeOut();
                 $(".answer-1").typed({
-                    strings: ["Good question.", "Art is the highest expression of human intellect."],
+                    strings: ["Good question.", "1. Art is the highest expression of human intellect."],
+                    typeSpeed: 0
+                });
+                $(".answer-2").typed({
+                    strings: ["2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fugiat minima accusantium laudantium animi, quis commodi illum, dolore pariatur suscipit odio obcaecati blanditiis repellendus cupiditate error quidem est delectus, laborum!"],
+                    typeSpeed: 0
+                });
+                $(".answer-3").typed({
+                    strings: ["3. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fugiat minima accusantium laudantium animi, quis commodi illum, dolore pariatur suscipit odio obcaecati blanditiis repellendus cupiditate error quidem est delectus, laborum!"],
+                    typeSpeed: 0
+                });
+                $(".answer-4").typed({
+                    strings: ["4. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fugiat minima accusantium laudantium animi, quis commodi illum, dolore pariatur suscipit odio obcaecati blanditiis repellendus cupiditate error quidem est delectus, laborum!"],
+                    typeSpeed: 0
+                });
+            },
+            
+            'what is art': function () {
+                //$(".hello").empty();
+                $(".typed-cursor").fadeOut();
+                $(".answer-1").typed({
+                    strings: ["Good question.", "1. Art is the highest expression of human intellect."],
                     typeSpeed: 0
                 });
             },
             'clear screen': function () {
                 $(".answers").empty();
+                $(".typed-cursor").fadeOut();
             }
         };
 
