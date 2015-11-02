d5fa8349ca7232f6ccac101cd81777e81de2e698
Pablo Cortez <pabloenoc@live.com>
2015-11-02 15:38:09 -0800

    Word wrap needs work

--- src/js/main.js
+++ src/js/main.js
@@ -5,27 +5,29 @@ $(function () {
     if (annyang) {
         var commands = {
             'say hello': function () {
+                $("#hello").slideDown();
                 $("#hello").typed({
-                    strings: ["Hello!", "I am a project created by Pablo for Mrs. Guild's AP Lang class."],
+                    strings: ["Hello!", "I'm Pablo's creative project for Mrs. Guild's AP Lang class."],
                     typeSpeed: 0
                 });
             },
-            'answer the questions' : function() {
-                $(".typed-cursor").fadeOut();
-                $(".answer-1").typed({
-                    strings: ["Good question.", "1. Art is the highest expression of human intellect."],
+            'answer the questions': function() {
+//                $(".typed-cursor").fadeOut();
+                $("#answer1, #answer2, #answer3, #answer4").scroll();
+                $("#answer1").typed({
+                    strings: ["1. Art is the highest expression of human intellect."],
                     typeSpeed: 0
                 });
-                $(".answer-2").typed({
-                    strings: ["2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fugiat minima accusantium laudantium animi, quis commodi illum, dolore pariatur suscipit odio obcaecati blanditiis repellendus cupiditate error quidem est delectus, laborum!"],
+                $("#answer2").typed({
+                    strings: ["2. What makes me creative is my ability to turn my ideas into products through code."],
                     typeSpeed: 0
                 });
-                $(".answer-3").typed({
-                    strings: ["3. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fugiat minima accusantium laudantium animi, quis commodi illum, dolore pariatur suscipit odio obcaecati blanditiis repellendus cupiditate error quidem est delectus, laborum!"],
+                $("#answer3").typed({
+                    strings: ["3. Art can teach us what we are capable of, it can show us our limits, and lack thereof."],
                     typeSpeed: 0
                 });
-                $(".answer-4").typed({
-                    strings: ["4. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fugiat minima accusantium laudantium animi, quis commodi illum, dolore pariatur suscipit odio obcaecati blanditiis repellendus cupiditate error quidem est delectus, laborum!"],
+                $("#answer4").typed({
+                    strings: ["4. Can art change the world? Yes, obviously. It is through artistic means that we have technological developments and scientific advances in our society."],
                     typeSpeed: 0
                 });
             },
