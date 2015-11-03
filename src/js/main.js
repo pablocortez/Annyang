47a836fd9b9f713563a2938dd7e926b4f8c16e59
Pablo Cortez <pabloenoc@live.com>
2015-11-02 18:12:16 -0800

    No cursor for answers.

--- src/js/main.js
+++ src/js/main.js
@@ -15,32 +15,35 @@ $(function () {
                 $("#answer1, #answer2, #answer3, #answer4").scroll();
                 $("#answer1").typed({
                     strings: ["1. Art is the highest expression of human intellect."],
-                    typeSpeed: 0
+                    typeSpeed: 0,
+                    showCursor: false
                 });
                 $("#answer2").typed({
-                    strings: ["2. What makes me creative is my ability to turn my ideas into products through code."],
-                    typeSpeed: 0
+                    strings: ["2. What makes me creative is my ability to turn my ideas into cool stuff, through code."],
+                    typeSpeed: 0,
+                    showCursor: false
                 });
                 $("#answer3").typed({
                     strings: ["3. Art can teach us what we are capable of, it can show us our limits, and lack thereof."],
-                    typeSpeed: 0
+                    typeSpeed: 0,
+                    showCursor: false
                 });
                 $("#answer4").typed({
                     strings: ["4. Can art change the world? Yes, obviously. It is through artistic means that we have technological developments and scientific advances in our society."],
-                    typeSpeed: 0
+                    typeSpeed: 0,
+                    showCursor: false
                 });
             },
             
             'what is art': function () {
-                //$(".hello").empty();
-                $(".typed-cursor").fadeOut();
-                $(".answer-1").typed({
-                    strings: ["Good question.", "1. Art is the highest expression of human intellect."],
-                    typeSpeed: 0
+                $("#answer1").typed({
+                    strings: ["1. Art is the highest expression of human intellect."],
+                    typeSpeed: 0,
+                    showCursor: false
                 });
             },
             'clear screen': function () {
-                $(".answers").empty();
+                $(".answer").empty();
                 $(".typed-cursor").fadeOut();
             }
         };
